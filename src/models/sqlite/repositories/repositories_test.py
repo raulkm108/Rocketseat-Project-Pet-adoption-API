0e0137f96ee0136af828ade19ee2ea4762bfaec8
raulkm108 <raulkm.civil@gmail.com>
2025-12-03 13:39:16 -0300

    Added a test for the delete_pet method

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,10 +1,17 @@
+import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 
 db_connection_handler.connect_to_db()
 
+@pytest.mark.skip(reason="Interation with the db")
 def test_list_pets():
     repo = PetsRepository(db_connection_handler)
     response = repo.list_pets()
     print()
     print(response)
+
+def test_delete_pet():
+    name = "belinha"
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
