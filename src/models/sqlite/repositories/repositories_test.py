a9e25dd9f7fcbe4b7d68ff1f7f770cadfe3d4985
raulkm108 <raulkm.civil@gmail.com>
2025-12-04 12:02:44 -0300

    Adding comment # to not create a connection everytime

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -2,7 +2,8 @@ import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 
-db_connection_handler.connect_to_db()
+
+# db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="Interation with the db")
 def test_list_pets():
@@ -11,6 +12,7 @@ def test_list_pets():
     print()
     print(response)
 
+@pytest.mark.skip(reason="Interation with the db")
 def test_delete_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
