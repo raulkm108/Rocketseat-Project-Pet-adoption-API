bfbb5a15f6066f54b72ecd2d845be7872c79112e
raulkm108 <raulkm.civil@gmail.com>
2025-12-04 18:55:33 -0300

    Added 2 errors tests
    Added 2 more tests, now to test the NoResult on the list_pets method, which we mock a connection to the db. And in the delete_pet_error we raise an exception and test if the rollback is happening

--- src/models/sqlite/repositories/pets_repository_test.py
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -2,6 +2,8 @@ from unittest import mock
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 from src.models.sqlite.entities.pets import PetsTable
 from .pets_repository import PetsRepository
+from sqlalchemy.orm.exc import NoResultFound
+import pytest
 
 class MockConnection:
     def __init__(self) -> None:
@@ -17,6 +19,20 @@ class MockConnection:
             ]
         )
 
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+class MockConnectionNoResult:
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+
+    def __raise_no_result_found(self, *args, **kwargs):
+        raise NoResultFound("No result found")
+    
     def __enter__(self):
         return self
     
@@ -45,3 +61,24 @@ def test_delete_pet():
     mock_connection.session.query.assert_called_once_with(PetsTable)
     mock_connection.session.filter.assert_called_once_with(PetsTable.name == "petName")
     mock_connection.session.delete.assert_called_once()
+
+def test_list_pets_no_result():
+    mock_connection = MockConnectionNoResult()
+    repo = PetsRepository(mock_connection)
+    response = repo.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+
+    assert response == []
+
+def test_delete_pet_error():
+    mock_connection = MockConnectionNoResult()
+    repo = PetsRepository(mock_connection)   
+
+    with pytest.raises(Exception):
+        repo.delete_pets("petName")
+
+    mock_connection.session.rollback.assert_called_once()
