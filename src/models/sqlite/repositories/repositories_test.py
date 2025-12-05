151ee4ad9c279be77b67fa375498f9ca0020733c
raulkm108 <raulkm.civil@gmail.com>
2025-12-04 21:55:56 -0300

    New test for people
    Test for the repositorie people

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,9 +1,10 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 
-# db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 @pytest.mark.skip(reason="Interation with the db")
 def test_list_pets():
@@ -17,3 +18,12 @@ def test_delete_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+    
+@pytest.mark.skip(reason="Interation with the db")
+def test_insert_person():
+    first_name = "test first name"
+    last_name = "test last name" 
+    age = 77
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age)
