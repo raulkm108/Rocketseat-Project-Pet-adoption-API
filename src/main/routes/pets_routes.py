c016567b8106ce36974d807b8a6f661e69e65de3
raulkm108 <raulkm.civil@gmail.com>
2026-09-23 12:06:59 -0300

    2 new routes
    Created the 2 new routes for the 2 methods, delete_pet and person_finder

--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from src.main.composer.pet_lister_composer import pet_lister_composer
+from src.main.composer.pet_deleter_composer import pet_deleter_composer
 from src.views.http_types.http_request import HttpRequest
 
 pet_route_bp = Blueprint("pet_routes", __name__)
@@ -8,6 +9,15 @@ pet_route_bp = Blueprint("pet_routes", __name__)
 def list_pets():
     http_request = HttpRequest()
     view = pet_lister_composer()
+
+    http_response = view.handle(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
+
+@pet_route_bp.route("/pet/<name>", methods=["DELETE"])
+def delete_pet(name):
+    http_request = HttpRequest(param={"name": name})
+    view = pet_deleter_composer()
 
     http_response = view.handle(http_request)
 
